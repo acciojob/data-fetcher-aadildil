52ea9302fdda8f1684f9f8a5a11603d569296ff0
aadildil <pmaadil102@gmail.com>
2023-11-04 16:40:37 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
+import axios from "axios";
+import { json } from "react-router-dom";
+
 
 const App = () => {
+
+  const [data,setData]=useState(null)
+  const [loading,setLoading]=useState(false)
+
+   function fetchData()
+  {
+    
+    setLoading(true)
+    
+      axios.get("https://dummyjson.com/products")
+      .then((response)=>{
+        setData(JSON.stringify(response.data,null,2))
+        setLoading(false)
+      })
+      .catch(error=>console.log(error))
+    
+
+  }
+  useEffect(()=>{
+
+    fetchData();
+
+  },[])
   return (
     <div>
-        {/* Do not remove the main div */}
+
+        {
+          loading?<p>loading...</p>:<div>
+            <h1>Data Fetched from API</h1>
+            <pre>
+              {data}
+            </pre>
+          </div>
+        }
     </div>
   )
 }
